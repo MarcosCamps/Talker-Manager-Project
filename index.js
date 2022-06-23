66424beb0ccda4f6d5eedf0cbec1ea896ada4317
Marcos Campos <marxmyers@icloud.com>
2022-06-22 21:06:59 -0300

    Sending requirement 5

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const bodyParser = require('body-parser');
 const { randomBytes } = require('crypto');
 const readFIle = require('./helpers/readFile');
 const loginMiddleware = require('./middlewares/loginMiddleware');
+const authorizationMiddleware = require('./middlewares/authozirationMiddleware');
+const nameMiddleware = require('./middlewares/nameMiddleware');
+const ageMiddleware = require('./middlewares/ageMiddleware');
+const talkerMiddleware = require('./middlewares/talkerMiddleware');
+const watchedAtMiddleware = require('./middlewares/watchedAtMiddleware');
+const rateMiddleware = require('./middlewares/rateMiddleware');
+const writeFIle = require('./helpers/writeFile');
 
 const app = express();
 app.use(bodyParser.json());
@@ -20,7 +27,7 @@ app.get('/talker', async (_req, res) => {
     const talker = await readFIle();
     res.status(200).json(talker);
   } catch (error) {
-   res.status(400).json({ message: error });
+    res.status(400).json({ message: error });
   }
 });
 
@@ -40,6 +47,27 @@ app.post('/login', loginMiddleware, (_req, res) => {
   const token = randomBytes(8).toString('hex');
   res.status(200).json({ token });
 });
+
+app.post('/talker',
+  authorizationMiddleware,
+  nameMiddleware,
+  ageMiddleware,
+  talkerMiddleware,
+  watchedAtMiddleware,
+  rateMiddleware,
+  async (req, res) => {
+    const { name, age, talk } = req.body;
+    try {
+      const talker = await readFIle();
+      const talkerPerson = { id: talker.length + 1, name, age, talk };
+      talker.push(talkerPerson);
+      // talker.find((person) => person.length - 1);
+      writeFIle(talker);
+      res.status(201).json(talkerPerson);
+    } catch (error) {
+      res.status(400).json({ message: error });
+    }
+  });
 
 app.listen(PORT, () => {
   console.log('Online');
---
+++ middlewares/talkerMiddleware.js
@@ -0,0 +1,11 @@
+const talkerMiddleware = (req, res, next) => {
+  const { talk } = req.body;
+  if (!talk) {
+    return res.status(400).json({
+      message: 'O campo "talk" é obrigatório',
+    });
+  }
+  return next();
+};
+
+module.exports = talkerMiddleware;
